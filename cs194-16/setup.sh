990d82930ed6a590069894fb5458b1165f510bb0
Kay Ousterhout <kayousterhout@gmail.com>
2014-04-06 21:31:13 -0700

    Fixes to setup script

--- cs194-16/setup.sh
+++ cs194-16/setup.sh
@@ -3,7 +3,9 @@
 chmod u+x /root/cs194-16/machine_setup.sh
 /root/spark-ec2/copy-dir /root/cs194-16
 
+SLAVES=`cat /root/spark-ec2/slaves`
+
 for slave in $SLAVES; do
     # Kill all existing screens (to kill any existing ipython notebooks) and then setup the machine.
-    ssh $slave StrictHostKeyChecking=no -o ConnectTimeout=5 "/root/cs194-16/machine_setup.sh"
+    ssh -o StrictHostKeyChecking=no -o ConnectTimeout=5 $slave "/root/cs194-16/machine_setup.sh"
 done
