8240ec58cfeb665690842214abbb994fba703f1c
Kay Ousterhout <kayousterhout@gmail.com>
2014-04-06 20:08:24 -0700

    Stripped machine learning stuff out of training setup

--- training/setup.sh
+++ training/setup.sh
@@ -10,16 +10,6 @@ mount -t ext4 /dev/sdf /ampcamp-data
 
 # Clone and copy training repo
 ssh-keyscan -H github.com >> /root/.ssh/known_hosts
-rm -rf training
-git clone https://github.com/amplab/training.git -b ampcamp4
-
-pushd training
-/root/spark-ec2/copy-dir /root/training
-
-ln -T -f -s /root/training/streaming /root/streaming
-ln -T -f -s /root/training/machine-learning /root/machine-learning
-ln -T -f -s /root/training/java-app-template /root/java-app-template
-ln -T -f -s /root/training/scala-app-template /root/scala-app-template
 
 # Add hdfs to the classpath
 cp /root/ephemeral-hdfs/conf/core-site.xml /root/spark/conf/
