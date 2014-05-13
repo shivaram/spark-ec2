6b15db14ff3a8f80234891b641c28d69f33daa53
Shivaram Venkataraman <shivaram@eecs.berkeley.edu>
2014-05-12 17:30:04 -0700

    Dont copy imagenet data

--- pipelines/setup.sh
+++ pipelines/setup.sh
@@ -10,8 +10,8 @@ sbt/sbt assembly
 # Copy training data
 /root/ephemeral-hdfs/bin/stop-mapred.sh
 /root/ephemeral-hdfs/bin/start-mapred.sh
-/root/ephemeral-hdfs/bin/hadoop distcp s3n://imagenet-train/ /imagenet-train
-/root/ephemeral-hdfs/bin/hadoop distcp s3n://imagenet-validation/ /imagenet-validation
+#/root/ephemeral-hdfs/bin/hadoop distcp s3n://imagenet-train/ /imagenet-train
+#/root/ephemeral-hdfs/bin/hadoop distcp s3n://imagenet-validation/ /imagenet-validation
 
 # Setup lib and conf correctly
 cp /root/ephemeral-hdfs/conf/core-site.xml /root/pipelines/conf/
