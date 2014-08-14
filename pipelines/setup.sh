fc759351140128cf9d821c937d2f019de66b44e2
Shivaram Venkataraman <shivaram@cs.berkeley.edu>
2014-08-14 14:30:25 -0700

    Add support for using Spark 1.1

--- pipelines/setup.sh
+++ pipelines/setup.sh
@@ -21,3 +21,38 @@ cp /root/pipelines/lib/openblas/libjblas.so /root/pipelines/lib/
 /root/spark-ec2/copy-dir /root/pipelines/lib
 
 popd
+
+# Replace the spark setup with our own Spark
+pushd /root
+s3cmd get s3://spark1.1-pipelines/spark-1.1-pipelines.tar.gz
+s3cmd get s3://spark1.1-pipelines/spark-1.1-pipelines-m2.tar.gz
+
+# Now for m2
+mkdir -p /tmp/test
+pushd /tmp/test
+  tar -xf /root/spark-1.1-pipelines-m2.tar.gz
+  pushd root
+    cp -r .m2/repository/org/apache/spark/* ~/.m2/repository/org/apache/spark/
+  popd
+popd
+rm -rf /tmp/test
+
+# Get the binary dist right
+mv /root/spark /root/spark-old
+
+mkdir -p /tmp/test
+pushd /tmp/test
+  tar -xf spark-1.1-pipelines.tar.gz
+  cp /root/spark-old/conf/* ./spark/conf/
+  mv ./spark /root/spark
+popd
+
+rm -rf /tmp/test
+
+/root/ephemeral-hdfs/bin/slaves.sh rm -rf /root/spark
+/root/spark-ec2/copy-dir /root/spark
+
+spark-1.1-pipelines.tar.gz
+spark-1.1-pipelines-m2.tar.gz
+
+popd
