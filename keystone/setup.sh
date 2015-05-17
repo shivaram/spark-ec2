080c5ed71dcc8b25e198c74800204fc01ecc5008
Shivaram Venkataraman <shivaram@cs.berkeley.edu>
2015-05-16 22:12:56 -0700

    Use spark/sbin/slaves.sh

--- keystone/setup.sh
+++ keystone/setup.sh
@@ -30,10 +30,10 @@ fi
 /root/matrix-bench/build-openblas-ec2.sh
 /root/spark-ec2/copy-dir /root/openblas-install
 
-~/ephemeral-hdfs/bin/slaves.sh rm /etc/ld.so.conf.d/atlas-x86_64.conf
-~/ephemeral-hdfs/bin/slaves.sh ldconfig
-~/ephemeral-hdfs/bin/slaves.sh ln -sf /root/openblas-install/lib/libopenblas.so /usr/lib64/liblapack.so.3
-~/ephemeral-hdfs/bin/slaves.sh ln -sf /root/openblas-install/lib/libopenblas.so /usr/lib64/libblas.so.3
+~/spark/sbin/slaves.sh rm /etc/ld.so.conf.d/atlas-x86_64.conf
+~/spark/sbin/slaves.sh ldconfig
+~/spark/sbin/slaves.sh ln -sf /root/openblas-install/lib/libopenblas.so /usr/lib64/liblapack.so.3
+~/spark/sbin/slaves.sh ln -sf /root/openblas-install/lib/libopenblas.so /usr/lib64/libblas.so.3
 
 rm /etc/ld.so.conf.d/atlas-x86_64.conf
 ldconfig
@@ -42,8 +42,8 @@ ln -sf /root/openblas-install/lib/libopenblas.so /usr/lib64/libblas.so.3
 
 /root/spark/sbin/stop-all.sh
 
-~/ephemeral-hdfs/bin/slaves.sh rm -rf /root/spark/work
-~/ephemeral-hdfs/bin/slaves.sh mkdir -p /mnt/spark-work 
-~/ephemeral-hdfs/bin/slaves.sh ln -s /mnt/spark-work /root/spark/work
+~/spark/sbin/slaves.sh rm -rf /root/spark/work
+~/spark/sbin/slaves.sh mkdir -p /mnt/spark-work 
+~/spark/sbin/slaves.sh ln -s /mnt/spark-work /root/spark/work
 
 /root/spark/sbin/start-all.sh
