3d2e8eb6689104b4f61aa48dc36ffbd200b5f8d3
Tomer Kaftan <tomer.kaftan@gmail.com>
2015-07-15 15:34:21 -0700

    Record my modifications to runvw.sh

---
+++ runvw.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+mapper=`printenv mapred_task_id | cut -d "_" -f 5`
+rm -f temp.cache
+date +"%F %T Start training mapper=$mapper" > /dev/stderr
+vwcmd="/root/vowpal_wabbit/vowpalwabbit/vw --total $mapred_map_tasks --node $mapper --cache_file temp.cache --span_server $mapreduce_job_submithost --loss_function=classic"
+mapred_job_id=`echo $mapred_job_id | tr -d 'job_'`
+gdcmd="$vwcmd --unique_id $mapred_job_id --passes 1 --adaptive -d /dev/stdin -f tempmodel --ect 3"
+mapred_job_id=`expr $mapred_job_id \* 2` #create new nonce
+bfgscmd="$vwcmd --unique_id $mapred_job_id --bfgs --mem 5 --passes 2 -f model -i tempmodel"
+if [ "$mapper" == '000000' ]; then
+    $gdcmd > mapperout 2>&1
+    if [ $? -ne 0 ]; then
+      date +"%F %T Failed mapper=$mapper cmd=$gdcmd" > /dev/stderr
+      exit 1
+    fi
+    $bfgscmd >> mapperout 2>&1
+    outfile=$mapred_output_dir/model
+    mapperfile=$mapred_output_dir/mapperout
+    found=`/root/ephemeral-hdfs/bin/hadoop dfs -lsr / | grep $mapred_output_dir | grep mapperout`
+    if [ "$found" != "" ]; then
+      /root/ephemeral-hdfs/bin/hadoop dfs -rm -r $mapperfile
+    fi
+    found=`/root/ephemeral-hdfs/bin/hadoop dfs -lsr / | grep $mapred_output_dir | grep model`
+    if [ "$found" != "" ]; then
+      /root/ephemeral-hdfs/bin/hadoop dfs -rm -r $outfile
+    fi
+    date +"%F %T outfile=$outfile" > /dev/stderr
+    /root/ephemeral-hdfs/bin/hadoop dfs -put model $outfile
+    /root/ephemeral-hdfs/bin/hadoop dfs -put mapperout $mapperfile
+else
+    $gdcmd
+    if [ $? -ne 0 ]; then
+      date +"%F %T Failed mapper=$mapper cmd=$gdcmd" > /dev/stderr
+      exit 1
+    fi
+    $bfgscmd
+fi
+date +"%F %T Done mapper=$mapper" > /dev/stderr
