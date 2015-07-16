3bd3eea71293f133d14a5aec25e3a4bb47aa75a9
Tomer Kaftan <tomer.kaftan@gmail.com>
2015-07-16 16:30:57 -0700

    First version of vwtimit script

---
+++ vwtimit.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+HADOOP=/root/ephemeral-hdfs/bin/hadoop
+
+/root/ephemeral-hdfs/bin/start-all.sh
+
+# Get the data from S3
+$HADOOP distcp s3n://timit-data/timit-train-features.csv /
+$HADOOP distcp s3n://timit-data/timit-train-labels.sparse /
+$HADOOP distcp s3n://timit-data/timit-test-features.csv /
+$HADOOP distcp s3n://timit-data/timit-test-labels.sparse /
+
+KEYSTONE_MEM=4g ./keystone/bin/run-pipeline.sh \
+  pipelines.speech.VWTimitFeaturizer \
+  --trainDataLocation /timit-train-features.csv \
+  --trainLabelsLocation /timit-train-labels.sparse \
+  --testDataLocation /timit-test-features.csv \
+  --testLabelsLocation /timit-test-labels.sparse \
+  --vwFeaturesWriteLocation /$TIMESTAMP/data
+  --numCosines 1
+
+/root/vowpal_wabbit/cluster/spanning_tree
+
+$HADOOP jar /root/ephemeral-hdfs/contrib/streaming/hadoop-streaming-1.0.4.jar \
+    -files runvw.sh \
+    -Dmapred.job.map.memory.mb=2000 \
+    -input /$TIMESTAMP/data \
+    -output /$TIMESTAMP/out \
+    -mapper runvw.sh \
+    -reducer NONE
+
+echo "wrote to hdfs /$TIMESTAMP/out"
