27fe48a8be54f98257e52164dc4ff65d908b1ee2
Tomer Kaftan <tomer.kaftan@gmail.com>
2015-07-16 19:08:10 -0700

    More fixes

--- vwtimit.sh
+++ vwtimit.sh
@@ -11,14 +11,17 @@ $HADOOP distcp s3n://timit-data/timit-train-features.csv /
 $HADOOP distcp s3n://timit-data/timit-train-labels.sparse /
 $HADOOP distcp s3n://timit-data/timit-test-features.csv /
 $HADOOP distcp s3n://timit-data/timit-test-labels.sparse /
+$HADOOP dfs -copyToLocal /timit-train-labels.sparse /
+$HADOOP dfs -copyToLocal /timit-test-labels.sparse /
 
-KEYSTONE_MEM=4g ./keystone/bin/run-pipeline.sh \
+export SPARK_HOME=/root/spark
+KEYSTONE_MEM=4g /root/keystone/bin/run-pipeline.sh \
   pipelines.speech.VWTimitFeaturizer \
   --trainDataLocation /timit-train-features.csv \
   --trainLabelsLocation /timit-train-labels.sparse \
   --testDataLocation /timit-test-features.csv \
   --testLabelsLocation /timit-test-labels.sparse \
-  --vwFeaturesWriteLocation /$TIMESTAMP/data
+  --vwFeaturesWriteLocation /$TIMESTAMP/data \
   --numCosines 1
 
 /root/vowpal_wabbit/cluster/spanning_tree
