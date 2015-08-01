0e4360d4d9bb3b52b7bb8e57d45853267ebc57c1
Tomer Kaftan <tomer.kaftan@gmail.com>
2015-08-01 16:54:10 -0700

    More changes

--- experiments/amazon/prepare.sh
+++ experiments/amazon/prepare.sh
@@ -6,7 +6,7 @@ HADOOP=/root/mapreduce/bin/hadoop
 /root/ephemeral-hdfs/sbin/start-all.sh
 
 # Get the data from S3
-$HADOOP distcp s3n://files.sparks.west/data/amazon /amazon/train
+$HADOOP distcp s3n://files.sparks.west/data/amazon/ /amazon/train
 /root/ephemeral-hdfs/bin/hadoop dfs -mkdir /amazon/test
 /root/ephemeral-hdfs/bin/hadoop dfs -mv /amazon/train/part-aam* /amazon/test/
 /root/ephemeral-hdfs/bin/hadoop dfs -mv /amazon/train/part-aal* /amazon/test/
