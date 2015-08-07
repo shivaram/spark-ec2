efc5887680c1a81444915b14bda1b8bd72e07018
Tomer Kaftan <tomer.kaftan@gmail.com>
2015-08-06 20:17:56 -0700

    Changed timit log regression to be two stages

--- experiments/timit/run-keystone-logistic-regression-trial.sh
+++ experiments/timit/run-keystone-logistic-regression-trial.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 set -e
 
+NUM_COSINES=${NUM_COSINES:-16}
+NUM_PARTS=${NUM_PARTS:-256}
+
+# Get the timestamp for this run
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+
 pushd /root/keystone > /dev/null
 export SPARK_HOME=/root/spark
 time KEYSTONE_MEM=97g /root/keystone/bin/run-pipeline.sh \
-  pipelines.speech.LogRegressionTimitPipeline \
+  pipelines.speech.LogisticRegressionTimitFeaturizer \
   --trainDataLocation /timit-train-features.csv \
   --trainLabelsLocation /timit-train-labels.sparse \
+  --trainOutLocation /$TIMESTAMP/train/data \
   --testDataLocation /timit-test-features.csv \
   --testLabelsLocation /timit-test-labels.sparse \
-  --numParts 512 \
-  --numCosines 16
+  --testOutLocation /$TIMESTAMP/test/data \
+  --numParts $NUM_PARTS \
+  --numCosines $NUM_COSINES
+
+time KEYSTONE_MEM=97g /root/keystone/bin/run-pipeline.sh pipelines.speech.LogisticRegressionTimitSolverEval \
+    --trainDataLocation /$TIMESTAMP/train/data \
+    --testDataLocation /$TIMESTAMP/test/data
 
 popd > /dev/null
 
