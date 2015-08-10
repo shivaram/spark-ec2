2917976acbc1ad46a5235a9e32c1e9b3647a23f6
Tomer Kaftan <tomer.kaftan@gmail.com>
2015-08-10 11:05:46 -0700

    Added some scala fv stuff

---
+++ experiments/imagenet/run-keystone-scala-fv-scala-gmm-trial.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+IMAGENET_TRAIN_DIR="/imagenet-train-all-scaled-tar"
+IMAGENET_VAL_DIR="/imagenet-validation-all-scaled-tar"
+IMAGENET_LABELS="/mnt/imagenet-keystone/src/main/resources/imagenet-labels"
+
+pushd /mnt/imagenet-keystone > /dev/null
+
+export SPARK_HOME=/root/spark
+time KEYSTONE_MEM=97g /mnt/imagenet-keystone/bin/run-pipeline.sh \
+  pipelines.images.imagenet.LazyImageNetSiftLCSScalaFVScalaGMM \
+  --trainLocation $IMAGENET_TRAIN_DIR \
+  --testLocation $IMAGENET_VAL_DIR \
+  --labelPath $IMAGENET_LABELS \
+  --numPcaSamples 10000000 \
+  --numGmmSamples 10000000 \
+  --vocabSize 64 \
+  --centroidBatchSize 32
+
+popd > /dev/null
+
