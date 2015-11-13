74593ecd24c6674d0ab59de322e947c3a8e043eb
Tomer Kaftan <tomer.kaftan@gmail.com>
2015-11-13 11:13:41 -0800

    Fixed voc data write

--- experiments/voc/prepare.sh
+++ experiments/voc/prepare.sh
@@ -12,6 +12,7 @@ wget https://s3-us-west-2.amazonaws.com/voc-data/VOCtrainval_06-Nov-2007.tar
 wget https://s3-us-west-2.amazonaws.com/voc-data/VOCtest_06-Nov-2007.tar
 
 #Copy to HDFS
+/root/ephemeral-hdfs/bin/hadoop dfs -mkdir /data
 /root/ephemeral-hdfs/bin/hadoop dfs -copyFromLocal VOCtrainval_06-Nov-2007.tar /data/
 /root/ephemeral-hdfs/bin/hadoop dfs -copyFromLocal VOCtest_06-Nov-2007.tar /data/
 
