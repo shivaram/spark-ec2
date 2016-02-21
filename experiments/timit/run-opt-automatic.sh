57fad7285a03bdcce293f54fdba75f64a3d7001a
Tomer Kaftan <tomer.kaftan@gmail.com>
2016-02-21 14:31:18 -0800

    Added automatic profiling optimizations for other keystone branch

---
+++ experiments/timit/run-opt-automatic.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+NUM_PARTS=${NUM_PARTS:-256}
+
+pushd /root/keystone > /dev/null
+export SPARK_HOME=/root/spark
+time KEYSTONE_MEM=97g /root/keystone/bin/run-pipeline.sh \
+  pipelines.speech.TimitPipelineAutomaticOptimization \
+  --trainDataLocation /timit-train-features.csv \
+  --trainLabelsLocation /timit-train-labels.sparse \
+  --testDataLocation /timit-test-features.csv \
+  --testLabelsLocation /timit-test-labels.sparse \
+  --numParts $NUM_PARTS \
+  --numCosines 16 \
+  --numEpochs 3
+
+popd > /dev/null
+
