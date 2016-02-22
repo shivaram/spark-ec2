5771a9e0703cf0614e6a5491ac93ba2c316bcfa4
Tomer Kaftan <tomer.kaftan@gmail.com>
2016-02-22 12:58:14 -0800

    Added SystemML scripts

---
+++ experiments/amazon/run-systemml-solver.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+NUM_FEATURES=${NUM_FEATURES:-1024}
+NUM_PARTS=${NUM_PARTS:-256}
+
+pushd /root/keystone > /dev/null
+export SPARK_HOME=/root/spark
+time KEYSTONE_MEM=97g /root/keystone/bin/run-pipeline.sh \
+  pipelines.text.AmazonLBFGSSolvePipeline \
+  --trainLocation /amazon/train \
+  --testLocation /amazon/test \
+  --scriptLocation /root/keystone/bin/LinearRegCG.dml \
+  --bOutLocation /dev/null \
+  --numParts $NUM_PARTS \
+  --commonFeatures $NUM_FEATURES
+
+popd > /dev/null
+
